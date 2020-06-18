943a6eabbdc982f55452f5341607f3a715c749d2
Elia Pitozzi <45312350+Aile13@users.noreply.github.com>
2020-06-18 22:00:23 +0200

    Change pdo object, from heroku db env variable.

--- ContactFrom_v4/form_file/query/TopicData.php
+++ ContactFrom_v4/form_file/query/TopicData.php
@@ -8,7 +8,17 @@ class TopicData {
     }
     public function connect(){
       try{
-        $this->connection = new PDO("mysql:host=localhost;dbname=sgb_smart", "root", '');
+          $db = parse_url(getenv("DATABASE_URL"));
+
+           $pdo = new PDO("pgsql:" . sprintf(
+                    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+                    $db["host"],
+                    $db["port"],
+                    $db["user"],
+                    $db["pass"],
+                    ltrim($db["path"], "/")
+));
+        $this->connection = $pdo;
 
         }catch(PDOException $e){
           echo "Error: " . $e->getMEssage();
